12d4343aed98a7c9c26274a1c777d62c4a80f670
lpodev <lpodevco@gmail.com>
2023-03-27 14:23:31 +0200

    Demand 3: Display cheap products and bulk OrderItems

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,6 @@
 class OrderItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
+
+  scope :bulk, -> (size = 100) { where('quantity >= ?', size) }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
   belongs_to :category
   has_many :order_items
+
+  scope :cheap, -> { where('price <= ?', 0.2) }
 end
