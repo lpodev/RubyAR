8acb18d81313c4a02aa1c5b77156a5618423bdb8
lpodev <lpodevco@gmail.com>
2023-03-31 08:59:03 +0200

    Demand 4: Validates client firstname, lastname and that an order has items

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,6 +2,8 @@ class Client < ActiveRecord::Base
   has_many :orders
   has_many :products, through: :orders
 
+  validates :firstname, :lastname, presence:true, length: {minimum: 2}
+
   def to_s
     "#{firstname} #{lastname}"
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,6 @@ class Order < ActiveRecord::Base
   belongs_to :client
   has_many :order_items
   has_many :products, through: :order_items
+
+  validates :order_items, length: { minimum: 1 }
 end
